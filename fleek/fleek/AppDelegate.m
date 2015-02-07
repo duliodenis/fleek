c079685125776ef24c87b03b4f012b8725cefef8
Dulio Denis <dulio.denis@yahoo.com>
2015-02-06 19:51:49 -0500

    Added Parse Framework

--- fleek/fleek/AppDelegate.m
+++ fleek/fleek/AppDelegate.m
@@ -7,11 +7,25 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [Parse setApplicationId:@"eSzBJHobsgvoUPKNg4iEbuZ2ce1RPFVmJdDPox1V" clientKey:@"BIicNk1hC9UNyfSjCaN51ahqSxk9LeeKF8w7EyhN"];
+    
+    // Parse Test Code that Saves the Empire State Building Point of Interest
+    PFObject *poi = [PFObject objectWithClassName:@"POI"];
+    [poi setObject:@"ESB" forKey:@"Name"];
+    [poi setObject:@40.74 forKey:@"Latitude"];
+    [poi setObject:@-73.98 forKey:@"Longitude"];
+    [poi save];
+    
+    return YES;
+}
 
 @end
