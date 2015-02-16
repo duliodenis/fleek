e40ada49ca82b4592dab9f76caaa5220812467db
Dulio Denis <dulio.denis@yahoo.com>
2015-02-16 13:44:08 -0500

    Allow user to save pin to Parse from Annotation

--- fleek/fleek/MapViewController.m
+++ fleek/fleek/MapViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import <CoreLocation/CoreLocation.h>
+#import <Parse/Parse.h>
 #import "MapViewController.h"
 #import "LocationData.h"
 #import "SearchResultsViewController.h"
+#import "LocationAnnotationView.h"
 
-@interface MapViewController () <CLLocationManagerDelegate>
+@interface MapViewController () <CLLocationManagerDelegate, MKMapViewDelegate, UIAlertViewDelegate>
 @property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) LocationAnnotationView *currentAnnotation;
 @end
 
 
@@ -20,6 +23,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.currentAnnotation = nil;
+    self.mapView.delegate = self;
     
     // LocationManager to enable reporting the user's position
     if ( [CLLocationManager locationServicesEnabled] ) {
@@ -62,6 +67,8 @@
                 NSLog(@"Warning: Region monitoring not supported on this device."); }
         }
     }
+    
+    [self loadPointsOfInterest];
 }
 
 
@@ -70,6 +77,27 @@
     MKCoordinateSpan span = MKCoordinateSpanMake(0.01, 0.01);
     MKCoordinateRegion region = MKCoordinateRegionMake(center, span);
     [self.mapView setRegion:region animated:YES];
+}
+
+
+#pragma mark - Parse POI Save and Load Methods
+
+- (void)loadPointsOfInterest {
+    // PFQuery for user's POI
+}
+
+
+- (void)savePointOfInterest {
+    PFObject *poi = [PFObject objectWithClassName:@"POI"];
+    [poi setObject:self.currentAnnotation.title forKey:@"Name"];
+//    [poi setObject:self.currentAnnotation.coordinate forKey:@"coordinate"];
+    [poi saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            NSLog(@"Success");
+        } else {
+            NSLog(@"Failure");
+        }
+    }];
 }
 
 
@@ -103,6 +131,65 @@
         [destinationVC setLocationData:locationData];
         [[NSNotificationCenter defaultCenter] postNotificationName:@"ReloadTableNotification" object:self];
     }];
+}
+
+
+#pragma mark - Annotation Methods
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    //first make sure the annotation is our custom class...
+    if ([view.annotation isKindOfClass:[LocationAnnotationView class]])
+    {
+        //cast the object to our custom class...
+        LocationAnnotationView *annotation = (LocationAnnotationView *)view.annotation;
+        self.currentAnnotation = annotation;
+    
+        //show one alert view with title set to annotation's title
+        //and message set to annotation's subtitle...
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:[NSString stringWithFormat:@"Save %@", annotation.title]
+                              message:@"Would you like to save this to your favorites?"
+                              delegate:self
+                              cancelButtonTitle:@"Cancel"
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+    }
+}
+
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+    
+    MKPinAnnotationView *myPin=[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"current"];
+    
+    UIButton *calloutButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    UIButton *directionsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    directionsButton.frame = CGRectMake(0, 0, 23, 23);
+    [directionsButton setBackgroundImage:[UIImage imageNamed:@"Atomic"] forState:UIControlStateNormal];
+    
+    myPin.leftCalloutAccessoryView = directionsButton;
+    myPin.rightCalloutAccessoryView = calloutButton;
+    myPin.draggable = NO;
+    myPin.highlighted = NO;
+    myPin.animatesDrop= YES;
+    myPin.canShowCallout = YES;
+    myPin.pinColor = MKPinAnnotationColorPurple;
+    
+    return myPin;
+}
+
+#pragma mark - UIAlertViewDelegate Methods
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == [alertView cancelButtonIndex]) {
+        NSLog(@"Canceled the Parse Save Operation");
+        self.currentAnnotation = nil;
+    }
+    else {
+        [self savePointOfInterest];
+    }
 }
 
 @end
