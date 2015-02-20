25d1a7278a28e8d22362c65b485a75c5672d7f30
Dulio Denis <dulio.denis@yahoo.com>
2015-02-20 11:18:34 -0500

    Added updating user nickname

--- fleek/fleek/NicknameViewController.m
+++ fleek/fleek/NicknameViewController.m
@@ -11,6 +11,8 @@
 #import "SWRevealViewController.h"
 
 @interface NicknameViewController () <SWRevealViewControllerDelegate>
+@property (nonatomic) NSString* nickname;
+@property (weak, nonatomic) IBOutlet UITextField *nicknameTextField;
 
 @end
 
@@ -21,6 +23,44 @@
     self.revealViewController.delegate = self;
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     
+    // PFQuery *query = [PFQuery queryWithClassName:@"User"];
+    PFQuery *query = [PFUser query];
+    PFUser *user = [PFUser currentUser];
+    [query whereKey:@"objectId" equalTo:user.objectId];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            if (objects.count) {
+                PFObject *user = [objects firstObject];
+                self.nickname = [user objectForKey:@"nickname"];
+                NSLog(@"User has the nickname: %@", self.nickname);
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.nicknameTextField.text = self.nickname;
+                });
+                
+            }
+            else NSLog(@"User has no nickname");
+        } else {
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            NSLog(@"Error: %@", errorString);
+        }
+    }];
+    
+}
+
+- (IBAction)updateNickname:(id)sender {
+    PFUser *user = [PFUser currentUser];
+    self.nickname = self.nicknameTextField.text;
+    [user setObject:self.nickname forKey:@"nickname"];
+    
+    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            NSLog(@"Updated nickname on Parse Successful.");
+        } else {
+            NSLog(@"Update nickname to Parse Failure.");
+        }
+    }];
     
 }
 
